afd5b3d4cfd6ce406ca100439fb8f8876e324465
Atloas <miter010@gmail.com>
2021-03-30 17:56:50 +0200

    Testable application

---
+++ src/main/java/com/agh/GitHubActionsTest2/A.java
@@ -0,0 +1,7 @@
+package com.agh.GitHubActionsTest2;
+
+public class A {
+    public String getString() {
+        return "String";
+    }
+}
---
+++ src/main/java/com/agh/GitHubActionsTest2/Main.java
@@ -0,0 +1,8 @@
+package com.agh.GitHubActionsTest2;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.print(a.getString());
+    }
+}
